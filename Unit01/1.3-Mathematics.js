208ce1e7de16c17e76b0c95d341d095e22792477
GrantBeaton <grant.beaton@stu.ocsb.ca>
2024-03-26 13:40:45 -0400

    1.3 final

---
+++ Unit01/1.3-Mathematics.js
@@ -0,0 +1,15 @@
+let celsius = 100;
+let fahrenheit = 0;
+celsius = celsius + 2;
+fahrenheit = (celsius*9/5) + 32;
+console.log(fahrenheit);
+
+farenheit = 77;
+celsius = (farenheit - 32) * 5/9;
+console.log(celsius);
+
+console.log("77 degrees Farenheit is",celsius, "degrees celsius.");
+
+let m = 5;
+m = (0 - 5)/(6+4);
+console.log(m);
