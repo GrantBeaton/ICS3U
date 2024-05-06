0a05dae78163d441b8d8c5c04c06b4798ce5a0eb
Grant Beaton <grant.beaton@stu.ocsb.ca>
2024-05-06 13:44:33 -0400

    Update 3.2-Strings.js

--- Unit03/3.2-Strings.js
+++ Unit03/3.2-Strings.js
@@ -17,19 +17,43 @@ function reverse(str){
     let word = "";
     while (length != -1) {
         word = word + str.charAt(length);
-        length = length - 1;
+        length --;
       }
       console.log(word);
 }
 reverse(str);
 
 //Task 2
-let letters = "gdsafssdadgyafudsagdsadhasdshgaasdh";
 function dragons_and_goblins(letters){
     let goblins = 0;
     let dragons = 0;
-    
-
-
+    let letter = 0;
+    while (letter < letters.length + 1) {
+        if (letters.charCodeAt(letter) == 100){
+            dragons += 1;
+            letter ++;
+        }
+        else if (letters.charCodeAt(letter) == 103){
+            goblins += 1;
+            letter ++;
+        }
+        else{
+            letter ++;
+        }
+    }
+    console.log("Goblins: " + goblins + " Dragons: " + dragons);
 }
-dragons_and_goblins(letters);
+dragons_and_goblins("gdsafssdadgyafudsagdsadhasdshgaasdh");
+
+//Task 3
+function add(str){
+    let number = 0;
+    let total = 0;
+    while (number < str.length + 1) {
+        number = Number(number);
+        total += number;
+        number ++;
+        }
+    return total;
+    }
+console.log(add("42581032445327902308"));
