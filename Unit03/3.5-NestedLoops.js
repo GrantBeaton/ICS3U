d107f44391bd1f479e5d8bd54e6a3ec4530940ef
Grant Beaton <grant.beaton@stu.ocsb.ca>
2024-05-10 10:42:06 -0400

    Started 3.5

--- Unit03/3.5-NestedLoops.js
+++ Unit03/3.5-NestedLoops.js
@@ -10,12 +10,150 @@ const prompt = require('prompt-sync')();
 
 //Task 1
 function print_line(){
+    let whichone = Number(prompt("Emoji selection menu\n1. 🙀\n2. 🛟\n3. 👑\n4. 😎\n5. ❤️\nEnter the number of the emoji that you want: "));
+    let thisone = "";
+        if (whichone == 1){
+            thisone = "🙀";
+        }
+        else if (whichone == 2){
+            thisone = "🛟";
+        }
+        else if (whichone == 3){
+            thisone = "👑";
+        }
+        else if (whichone == 4){
+            thisone = "😎";
+        }
+        else if (whichone == 5){
+            thisone = "❤️";
+        }
+        else{
+            console.log("Alright, you don't get any emojis then. ");
+        }
     let howmany = Number(prompt("How many emojis do you want? "));
+    let oghowmany = howmany;
     let emoji = "";
+    let emoji2 = "";
     while (howmany > 0){
-        emoji = emoji + "🙀";
+        emoji += thisone;
         howmany -= 1;
     }
     console.log(emoji);
+    howmany = Number(prompt("How many emojis do you want (again): "));
+    do{
+        while (oghowmany > 0){
+            emoji2 += thisone;
+            oghowmany -= 1;
+        }
+        console.log(emoji);
+    }
+    while (oghowmany < 0);
 }
 print_line();
+
+//Task 2
+function print_square(){
+    let whichone = Number(prompt("Emoji selection menu\n1. ⚔️\n2. 🫏\n3. 🤮\n4. 💩\n5. 🦥\nEnter the number of the emoji that you want: "));
+    let thisone = "";
+        if (whichone == 1){
+            thisone = "⚔️";
+        }
+        else if (whichone == 2){
+            thisone = "🫏";
+        }
+        else if (whichone == 3){
+            thisone = "🤮";
+        }
+        else if (whichone == 4){
+            thisone = "💩";
+        }
+        else if (whichone == 5){
+            thisone = "🦥";
+        }
+        else{
+            console.log("Alright, you don't get any emojis then. ");
+        }
+    let howmany = Number(prompt("How wide do you want your square to be? "));
+    let oghowmany = howmany;
+    let trytwo = "";
+    let ogtrytwo = "";
+    let emoji = "";
+    let emoji2 = "";
+    while (howmany > 0){
+        emoji += thisone;
+        howmany -= 1;
+    }
+    while (oghowmany > 0){
+        console.log(emoji);
+        oghowmany -= 1;
+    }
+    
+    trytwo = Number(prompt("How wide do you want your square to be (again)? "));
+    ogtrytwo = trytwo;
+    do{
+        while (trytwo > 0){
+            emoji2 += thisone;
+            trytwo -= 1;
+        }
+        while (ogtrytwo > 0){
+            console.log(emoji);
+            ogtrytwo -= 1;
+        }
+    }
+    while (trytwo < 0);
+}
+print_square()
+
+//Bonus Task 1
+function print_diamond(){
+    let whichone = Number(prompt("Emoji selection menu\n1. 🤑\n2. 😜\n3. 😭\n4. 🤯\n5. 🤬\nEnter the number of the emoji that you want: "));
+    let thisone = "";
+        if (whichone == 1){
+            thisone = "🤑";
+        }
+        else if (whichone == 2){
+            thisone = "😜";
+        }
+        else if (whichone == 3){
+            thisone = "😭";
+        }
+        else if (whichone == 4){
+            thisone = "🤯";
+        }
+        else if (whichone == 5){
+            thisone = "🤬";
+        }
+        else{
+            console.log("Alright, you don't get any emojis then. ");
+        }
+    let howmany = Number(prompt("How wide do you want your diamond to be? "));
+    let emojicount = 1;
+    let emoji = " ";
+    let emoji2 = " ";
+        while (emojicount < (howmany + 1)){
+            emoji += thisone;
+            emojicount += 1;
+            console.log(emoji);
+        }
+        while (emojicount > 0){
+            emoji -= thisone;
+            emojicount -= 1;
+            console.log(emoji);
+        }
+    howmany = Number(prompt("How wide do you want your diamond to be (again)? "));
+    do{
+        while (emojicount < (howmany + 1)){
+            emoji2 += thisone;
+            emojicount += 1;
+            console.log(emoji2);
+        }
+        while (emojicount > 0){
+            emoji2 -= thisone;
+            emojicount -= 1;
+            console.log(emoji2);
+        }
+    }
+    while (howmany < 0)
+
+}
+print_diamond()
